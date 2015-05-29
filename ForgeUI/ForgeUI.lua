55e2096bec3c571dfec0086a806ee1a884a47e83
Adam Jedlička <adajedlicka@gmail.com>
2015-05-29 13:16:48 +0200

    v0.4.4

--- ForgeUI/ForgeUI.lua
+++ ForgeUI/ForgeUI.lua
@@ -50,8 +50,8 @@ function ForgeUI:new(o)
 	
 	 -- mandatory 
     self.api_version = 2
-	self.sVersion = "0.4.3g"
-	self.nVersion = 5
+	self.sVersion = "0.4.4"
+	self.nVersion = 6
 
 	self.author = "WintyBadass"
 	self.strAddonName = "~ForgeUI"
