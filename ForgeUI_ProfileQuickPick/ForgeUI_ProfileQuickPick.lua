42e6f8c5a64b2683e886fef3fb25d330f083a88e
chaarp <eivind@skum.no>
2015-10-14 20:02:33 +0200

    a lot of corrections. mover is still not working though

--- ForgeUI_ProfileQuickPick/ForgeUI_ProfileQuickPick.lua
+++ ForgeUI_ProfileQuickPick/ForgeUI_ProfileQuickPick.lua
@@ -11,83 +11,64 @@ local G = _G["ForgeLibs"]["ForgeGUI"] -- ForgeGUI
 
 -----------------------------------------------------------------------------------------------
 -- ForgeUI_ProfileQuickPick Module Definition
------------------------------------------------------------------------------------------------
-local ForgeUI_ProfileQuickPick = {} 
- 
------------------------------------------------------------------------------------------------
--- Constants
------------------------------------------------------------------------------------------------
--- e.g. local kiExampleVariableMax = 999
- 
------------------------------------------------------------------------------------------------
--- Initialization
------------------------------------------------------------------------------------------------
-function ForgeUI_ProfileQuickPick:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self 
+----------------------------------------------------------------------------------------------- 
+local ForgeUI_ProfileQuickPick = {
+	_NAME = "ForgeUI_ProfileQuickPick",
+  	_API_VERSION = 3,
+	_VERSION = "0.1",
+	DISPLAY_NAME = "Profile quick pick",
 
-    -- initialize variables here
+	tSettings = {}	
+	
+}
 
-    return o
-end
-
-function ForgeUI_ProfileQuickPick:Init()
-	local bHasConfigureFunction = false
-	local strConfigureButtonText = ""
-	local tDependencies = {
-		-- "UnitOrPackageName",
-	}
-    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
-end
- 
-
------------------------------------------------------------------------------------------------
--- ForgeUI_ProfileQuickPick OnLoad
------------------------------------------------------------------------------------------------
-function ForgeUI_ProfileQuickPick:OnLoad()
+function ForgeUI_ProfileQuickPick:ForgeAPI_Init()
     -- load our form file
-	self.xmlDoc = XmlDoc.CreateFromFile("ForgeUI_ProfileQuickPick.xml")
+	self.xmlDoc = XmlDoc.CreateFromFile("..//ForgeUI_ProfileQuickPick//ForgeUI_ProfileQuickPick.xml")
 	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
 end
 
------------------------------------------------------------------------------------------------
--- ForgeUI_ProfileQuickPick OnDocLoaded
------------------------------------------------------------------------------------------------
-function ForgeUI_ProfileQuickPick:OnDocLoaded()
+function ForgeUI_ProfileQuickPick:ForgeAPI_LoadSettings()
+	--F:API_RegisterMover(self, self.wndMain, "Something here", "Profile picker", "general", { bNameAsTooltip = true})	
+end
 
-	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
-	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "ForgeUI_ProfileQuickPickForm", nil, self)
-		if self.wndMain == nil then
-			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
-			return
-		end
+function ForgeUI_ProfileQuickPick:ForgeAPI_PopulateOptions()
+end
+
+function ForgeUI_ProfileQuickPick:OnDocLoaded()
+	if self.xmlDoc == nil and not self.xmlDoc:IsLoaded() then return end
+
+	self.wndMain = Apollo.LoadForm(self.xmlDoc, "PQPContainer", nil, self);
 		
-		self:LoadDropdown();
+	self:LoadDropdown();
 		
-	    self.wndMain:Show(true, true)		
-		
-		ChatSystemLib.PostOnChannel(2, "0.1");
-	end
+	self.wndMain:Show(true, true);
+	
+	ChatSystemLib.PostOnChannel(2, "Profilepicker loaded");
 end
 
 function ForgeUI_ProfileQuickPick:LoadDropdown()
 
-	local wndCombo = G:API_AddComboBox(tModule, wndProfiles, "Select profile", nil, nil, {
+	local wndCombo = G:API_AddComboBox(tModule, self.wndMain, "Select profile", nil, nil, {
 		fnCallback = self.OnSelectProfile,
 		tWidths = { 200, 0 },
-		tMove = { 0, 60 },
+tMove = { -3, -3 },
 		bInnerText = true,
 	})
 
 	for k, v in pairs(F:API_GetProfiles()) do
 		G:API_AddOptionToComboBox(self, wndCombo, v, v)
 	end
+	
+	ChatSystemLib.PostOnChannel(2, "Combobox loaded");
+
+	
 end
 
 function ForgeUI_ProfileQuickPick:OnSelectProfile(vValue, strKey)
 	F:API_ChangeProfile(vValue)
 end
 
-local ForgeUI_ProfileQuickPickInst = ForgeUI_ProfileQuickPick:new()
-ForgeUI_ProfileQuickPickInst:Init()
+--local ForgeUI_ProfileQuickPickInst = ForgeUI_ProfileQuickPick:new()
+--ForgeUI_ProfileQuickPickInst:Init()
+F:API_NewAddon(ForgeUI_ProfileQuickPick);
