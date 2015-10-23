c7419f1e648ea5f59ef05169c1c22f1a362a6c1e
cengie <marc2505@gmx.net>
2015-10-23 22:51:47 +0200

    Fix for issue #173
    Updated ImprovedSalvage fixes issue #173 together with the add sort-checkbox in the xml

--- ForgeUI_Inventory/ImprovedSalvage.lua
+++ ForgeUI_Inventory/ImprovedSalvage.lua
@@ -23,8 +23,34 @@ function ImprovedSalvage:new(o)
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
+	o.bSortByQuality = true
+	o.nStartIndex = 1
+	o.bFirst = true
 
 	return o
+end
+
+function ImprovedSalvage:OnSave(eType)
+	if eType ~= GameLib.CodeEnumAddonSaveLevel.Character then
+		return
+	end
+
+	local tSavedData =
+	{
+		bSortByQuality = self.bSortByQuality
+	}
+
+	return tSavedData
+end
+
+function ImprovedSalvage:OnRestore(eType, tSavedData)
+	if eType ~= GameLib.CodeEnumAddonSaveLevel.Character then
+		return
+	end
+
+	if tSavedData.bSortByQuality ~= nil then
+		self.bSortByQuality = tSavedData.bSortByQuality
+	end
 end
 
 function ImprovedSalvage:OnLoad()
@@ -37,13 +63,10 @@ function ImprovedSalvage:OnDocumentReady()
 		return
 	end
 	
-	Apollo.RegisterEventHandler("WindowManagementReady", 	"OnWindowManagementReady", self)
-	
 	Apollo.RegisterEventHandler("RequestSalvageAll", "OnSalvageAll", self) -- using this for bag changes
 	Apollo.RegisterSlashCommand("salvageall", "OnSalvageAll", self)
 
 	self.wndMain = Apollo.LoadForm(self.xmlDoc, "ImprovedSalvageForm", nil, self)
-	self.wndItemDisplay = self.wndMain:FindChild("ItemDisplayWindow")
 	
 	if self.locSavedWindowLoc then
 		self.wndMain:MoveToLocation(self.locSavedWindowLoc)
@@ -51,19 +74,23 @@ function ImprovedSalvage:OnDocumentReady()
 	
 	self.tContents = self.wndMain:FindChild("HiddenBagWindow")
 	self.arItemList = nil
-	self.nItemIndex = nil
+	self.tSelection = { nIndex = nil, nBagPos = nil}
 
 	self.wndMain:Show(false, true)
+
+	Apollo.RegisterEventHandler("WindowManagementReady", "OnWindowManagementReady", self)
+	self:OnWindowManagementReady()
 end
 
 function ImprovedSalvage:OnWindowManagementReady()
+	Event_FireGenericEvent("WindowManagementRegister", {strName = Apollo.GetString("CRB_Salvage")})
 	Event_FireGenericEvent("WindowManagementAdd", {wnd = self.wndMain, strName = Apollo.GetString("CRB_Salvage")})
 end
 
 --------------------//-----------------------------
 function ImprovedSalvage:OnSalvageAll()
 	self.arItemList = {}
-	self.nItemIndex = 1
+	self.tSelection = { nIndex = self.nStartIndex, nBagPos = self.nStartIndex}
 	
 	local tInvItems = GameLib.GetPlayerUnit():GetInventoryItems()
 	for idx, tItem in ipairs(tInvItems) do
@@ -72,6 +99,7 @@ function ImprovedSalvage:OnSalvageAll()
 		end
 	end
 
+	self.wndMain:FindChild("SortQualityBtn"):SetCheck(self.bSortByQuality)
 	self:RedrawAll()
 end
 
@@ -79,12 +107,16 @@ function ImprovedSalvage:OnSalvageListItemCheck(wndHandler, wndControl)
 	if not wndHandler or not wndHandler:GetData() then
 		return
 	end
-	
-	self.nItemIndex = wndHandler:GetData().nIdx
-	
-	local itemCurr = self.arItemList[self.nItemIndex]
+	local tData = wndHandler:GetData()
+	local itemCurr = self.arItemList[tData.nBagPos]
+	self.tSelection = { nIndex = tData.nIdx, nBagPos = tData.nBagPos}
 	self.wndMain:SetData(itemCurr)
 	self.wndMain:FindChild("SalvageBtn"):SetActionData(GameLib.CodeEnumConfirmButtonType.SalvageItem, itemCurr:GetInventoryId())
+end
+
+function ImprovedSalvage:OnSortQualityCheck()
+	self.bSortByQuality = self.wndMain:FindChild("SortQualityBtn"):IsChecked()
+	self:SortItems()
 end
 
 function ImprovedSalvage:OnSalvageListItemGenerateTooltip(wndControl, wndHandler) -- wndHandler is VendorListItemIcon
@@ -110,17 +142,14 @@ function ImprovedSalvage:OnSalvageListItemGenerateTooltip(wndControl, wndHandler
 end
 
 function ImprovedSalvage:RedrawAll()
-	local itemCurr = self.arItemList[self.nItemIndex]
-	
-	if itemCurr ~= nil then
+	if #self.arItemList > 0 then
 		local wndParent = self.wndMain:FindChild("MainScroll")
 		local nScrollPos = wndParent:GetVScrollPos()
 		wndParent:DestroyChildren()
 		
 		for idx, tItem in ipairs(self.arItemList) do
 			local wndCurr = Apollo.LoadForm(self.xmlDoc, "SalvageListItem", wndParent, self)
-			wndCurr:FindChild("SalvageListItemBtn"):SetData({nIdx = idx, tItem=tItem})
-			wndCurr:FindChild("SalvageListItemBtn"):SetCheck(idx == self.nItemIndex)
+			wndCurr:FindChild("SalvageListItemBtn"):SetData({nIdx = nil, tItem = tItem, nBagPos = idx})
 			
 			wndCurr:FindChild("SalvageListItemTitle"):SetTextColor(karEvalColors[tItem:GetItemQuality()])
 			wndCurr:FindChild("SalvageListItemTitle"):SetText(tItem:GetName())
@@ -133,11 +162,9 @@ function ImprovedSalvage:RedrawAll()
 			wndCurr:FindChild("SalvageListItemIcon"):GetWindowSubclass():SetItem(tItem)
 		end
 		
-		wndParent:ArrangeChildrenVert(0)
+		self:SortItems()
 		wndParent:SetVScrollPos(nScrollPos)
 	
-		self.wndMain:SetData(itemCurr)
-		self.wndMain:FindChild("SalvageBtn"):SetActionData(GameLib.CodeEnumConfirmButtonType.SalvageItem, itemCurr:GetInventoryId())
 		self.wndMain:Show(true)
 		self.wndMain:ToFront()
 	else
@@ -146,18 +173,70 @@ function ImprovedSalvage:RedrawAll()
 	
 end
 
+function ImprovedSalvage:SortItems()
+	local wndParent = self.wndMain:FindChild("MainScroll")
+	local fnSort = nil
+	
+	if self.bSortByQuality then
+		fnSort = function(wndCon1, wndCon2)
+			local wndItem1 = wndCon1:FindChild("SalvageListItemBtn")
+			local wndItem2 = wndCon2:FindChild("SalvageListItemBtn")
+			if wndItem1:GetData().tItem:GetItemQuality() == wndItem2:GetData().tItem:GetItemQuality() then
+				return wndCon1:FindChild("SalvageListItemTitle"):GetText() < wndCon2:FindChild("SalvageListItemTitle"):GetText()
+			else
+				return wndItem1:GetData().tItem:GetItemQuality() < wndItem2:GetData().tItem:GetItemQuality()
+			end
+		end
+	else
+		fnSort = function(wndCon1, wndCon2)
+			return wndCon1:FindChild("SalvageListItemBtn"):GetData().nBagPos < wndCon2:FindChild("SalvageListItemBtn"):GetData().nBagPos
+		end
+	end
+	
+	wndParent:ArrangeChildrenVert(Window.CodeEnumArrangeOrigin.LeftOrTop, fnSort)
+	
+	local bNotFound = true
+	for idx, wndCurr in ipairs(wndParent:GetChildren()) do
+		local wndCurrBtn = wndCurr:FindChild("SalvageListItemBtn")
+		local tBtnData = wndCurrBtn:GetData()
+		wndCurrBtn:SetData({nIdx = idx, tItem = tBtnData.tItem, nBagPos = tBtnData.nBagPos})
+		local bSelected = self.bFirst
+		if self.bSalvaged and idx ~= self.tSelection.nIndex then
+			bSelected = false
+		else
+			bSelected = bSelected or (self.bSalvaged and idx == self.tSelection.nIndex)
+			bSelected = bSelected or (not self.bSortByQuality and idx == self.tSelection.nBagPos) or (self.bSortByQuality and tBtnData.nBagPos == self.tSelection.nIndex)
+		end
+		wndCurrBtn:SetCheck(bSelected and bNotFound)
+		if bSelected and bNotFound then
+			self.tSelection.nBagPos = tBtnData.nBagPos
+			self.tSelection.nIndex = idx
+			bNotFound = false
+			self.bFirst = false
+			self.bSalvaged = false
+		end
+	end
+	
+	local itemCurr = self.arItemList[self.tSelection.nBagPos]
+	if itemCurr then
+		self.wndMain:SetData(itemCurr)
+		self.wndMain:FindChild("SalvageBtn"):SetActionData(GameLib.CodeEnumConfirmButtonType.SalvageItem, itemCurr:GetInventoryId())
+	end
+end
+
 function ImprovedSalvage:HelperPrereqFailed(tCurrItem)
 	return tCurrItem and tCurrItem:IsEquippable() and not tCurrItem:CanEquip()
 end
 
 function ImprovedSalvage:OnSalvageCurr()
-	if self.nItemIndex == #self.arItemList then 
-		table.remove(self.arItemList, self.nItemIndex )
-		self.nItemIndex = self.nItemIndex - 1
+	Event_ShowTutorial(GameLib.CodeEnumTutorial.CharacterWindow)
+	if self.tSelection.nIndex == #self.arItemList then 
+		table.remove(self.arItemList, self.tSelection.nBagPos )
+		self.tSelection.nIndex = self.tSelection.nIndex - 1
 	else
-		table.remove(self.arItemList, self.nItemIndex )
+		table.remove(self.arItemList, self.tSelection.nBagPos )
 	end
-	
+	self.bSalvaged = true
 	self:RedrawAll()
 end
 
@@ -165,6 +244,7 @@ function ImprovedSalvage:OnCloseBtn()
 	self.arItemList = {}
 	self.wndMain:SetData(nil)
 	self.wndMain:Show(false)
+	self.nStartIndex = self.bSortByQuality and self.tSelection.nBagPos or self.tSelection.nIndex
 end
 
 ----------------globals----------------------------
