b6f94c586eb75238ca84772481785271eec6e3d6
Adam Jedlička <adajedlicka@gmail.com>
2016-06-29 15:44:27 +0200

    Updated TaxiSkin

--- ForgeUI/modules/skins/carbine/taxi.lua
+++ ForgeUI/modules/skins/carbine/taxi.lua
@@ -9,46 +9,45 @@
 local F = _G["ForgeLibs"]["ForgeUI"] -- ForgeUI API
 local Skins = F:API_GetModule("skins")
 
-local fnUseSkin
+function fnUseSkin(luaCaller)
+	if not luaCaller.tWndRefs.wndMain then return end
 
-local function LoadSkin()
-	local addon = Apollo.GetAddon("TaxiMap")
+	Skins:HandleFrame(luaCaller.tWndRefs.wndMain)
 	
-	F:PostHook(addon, "OnDocumentReady", fnUseSkin)
+	luaCaller.tWndRefs.wndMain:FindChild("MainFrame"):SetStyle("Border", false)
 	
-	fnUseSkin(addon)
+	luaCaller.tWndRefs.wndMain:FindChild("TitleText"):SetTextColor("FFFF0000")
+	luaCaller.tWndRefs.wndMain:FindChild("TitleText"):SetFont("Nameplates")
+	luaCaller.tWndRefs.wndMain:FindChild("Title"):SetName("Title")
+	
+	Skins:HandleTitle(luaCaller.tWndRefs.wndMain:FindChild("Title"))
+	
+	Skins:HandleFooter(luaCaller.tWndRefs.wndMain:FindChild("MetalFooter"))
+	
+	luaCaller.tWndRefs.wndMain:FindChild("BGArt"):SetSprite("ForgeUI_InnerWindow")
+	luaCaller.tWndRefs.wndMain:FindChild("BGArt"):SetStyle("Picture", true)
+	luaCaller.tWndRefs.wndMain:FindChild("BGArt"):SetBGColor("FFFFFFFF")
+	luaCaller.tWndRefs.wndMain:FindChild("BGArt"):SetAnchorOffsets(5, 30, -5, -35)
+	
+	luaCaller.tWndRefs.wndMain:FindChild("BG_Backer"):Show(false)
+	
+	luaCaller.tWndRefs.wndMain:FindChild("MapContainer"):SetAnchorOffsets(1, 1, -1, -1)
+
+	luaCaller.tWndRefs.wndMain:FindChild("ZoneToggle"):SetAnchorOffsets(14, -41, 159, -4)
+	luaCaller.tWndRefs.wndMain:FindChild("SubzoneToggle"):SetAnchorOffsets(166, -41, 311, -4)
+	
+	Skins:HandleButton(luaCaller.tWndRefs.wndMain:FindChild("CancelButton"))
+	luaCaller.tWndRefs.wndMain:FindChild("CancelButton"):SetAnchorOffsets(-100, -25, 0, 0)
+	
+	Skins:HandleCloseButton(luaCaller.tWndRefs.wndMain:FindChild("CloseButton"))
 end
 
-fnUseSkin = function(luaCaller)
-	if not luaCaller.wndMain and not bRun then return end
+function LoadSkin()
+	local addon = Apollo.GetAddon("TaxiMap")
 	
-	Skins:HandleFrame(luaCaller.wndMain)
+	F:PostHook(addon, "OnInvokeTaxiWindow", fnUseSkin)
 	
-	luaCaller.wndMain:FindChild("MainFrame"):SetStyle("Border", false)
-	
-	-- workaround for carbin's stupid name system
-	luaCaller.wndMain:FindChild("Title"):SetName("TitleOuter")
-	luaCaller.wndMain:FindChild("Title"):SetTextColor("FFFF0000")
-	luaCaller.wndMain:FindChild("Title"):SetFont("Nameplates")
-	luaCaller.wndMain:FindChild("TitleOuter"):SetName("Title")
-	
-	Skins:HandleTitle(luaCaller.wndMain:FindChild("Title"))
-	
-	Skins:HandleFooter(luaCaller.wndMain:FindChild("MetalFooter"))
-	
-	luaCaller.wndMain:FindChild("BGArt"):SetSprite("ForgeUI_InnerWindow")
-	luaCaller.wndMain:FindChild("BGArt"):SetStyle("Picture", true)
-	luaCaller.wndMain:FindChild("BGArt"):SetBGColor("FFFFFFFF")
-	luaCaller.wndMain:FindChild("BGArt"):SetAnchorOffsets(5, 30, -5, -35)
-	
-	luaCaller.wndMain:FindChild("BG_Backer"):Show(false)
-	
-	luaCaller.wndMain:FindChild("MapContainer"):SetAnchorOffsets(1, 1, -1, -1)
-	
-	Skins:HandleButton(luaCaller.wndMain:FindChild("CancelButton"))
-	luaCaller.wndMain:FindChild("CancelButton"):SetAnchorOffsets(-100, -25, 0, 0)
-	
-	Skins:HandleCloseButton(luaCaller.wndMain:FindChild("CloseButton"))	
+	fnUseSkin(addon)
 end
 
 Skins:NewCarbineSkin("TaxiMap", LoadSkin)
--- ForgeUI/modules/skins/skins.lua
+++ ForgeUI/modules/skins/skins.lua
@@ -33,8 +33,6 @@ local tSkins = {}
 -- Module functions
 -----------------------------------------------------------------------------------------------
 function Skins:ForgeAPI_Init()
-	if true then return end
-
 	for k, v in pairs(tSkins) do
 		if Apollo.GetAddon(k) and self._DB.char.tLoadSkins[k] then
 			v.fnLoadSkin()
