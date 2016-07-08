97d8af2af79912e4088ced3b56208ffcaf2761ac
Adam Jedlička <adajedlicka@gmail.com>
2016-07-08 19:59:15 +0200

    Hazards - Bugfixes & proper names for hazards

--- ForgeUI_Hazards/ForgeUI_Hazards.lua
+++ ForgeUI_Hazards/ForgeUI_Hazards.lua
@@ -37,6 +37,7 @@ local ForgeUI_Hazards = {
 	},
 
 	tHazards = {},
+	tQueuedHazards = {},
 }
 
 -----------------------------------------------------------------------------------------------
@@ -57,12 +58,29 @@ end
 function ForgeUI_Hazards:OnDocLoaded()
 	self.wndHazardsHolder = Apollo.LoadForm(self.xmlDoc, "HazardsHolder", F:API_GetStratum("Hud"), self)
 	F:API_RegisterMover(self, self.wndHazardsHolder, "Hazards", "Hazards", "misc", {})
+
+	for k, v in pairs(self.tQueuedHazards) do
+		self:CreateHazard(k, v.strName, v.nMin, v.nMax, v.crBar, v.tOptions)
+	end
+	self.tQueuedHazards = {}
 end
 
 -------------------------------------------------------------------------------
 -- Hazard functions
 -------------------------------------------------------------------------------
 function ForgeUI_Hazards:CreateHazard(nID, strName, nMin, nMax, crBar, tOptions)
+	if not self.wndHazardsHolder then
+		self.tQueuedHazards[nID] = {
+			strName = strName,
+			nMin = nMin,
+			nMax = nMax,
+			crBar = crBar,
+			tOptions = tOptions,
+		}
+
+		return
+	end
+
 	if self.tHazards[nID] ~= nil then 
 		self:RemoveHazard(nID)
 	end
@@ -97,6 +115,8 @@ function ForgeUI_Hazards:CreateHazard(nID, strName, nMin, nMax, crBar, tOptions)
 end
 
 function ForgeUI_Hazards:UpdateHazard(nID, tOptions)
+	if not self.wndHazardsHolder then return end
+
 	local wndHazard = self.tHazards[nID]
 	
 	if not tOptions then return end
@@ -145,7 +165,7 @@ end
 
 function ForgeUI_Hazards:OnHazardEnable(idHazard, strDisplayTxt)
 	local eHazardType = nil
-	local tData = nil
+	local tData = { wut = lol }
 	
 	for idx, tDat in ipairs(HazardsLib.GetHazardActiveList()) do
 		if tDat.nId == idHazard then
@@ -153,11 +173,11 @@ function ForgeUI_Hazards:OnHazardEnable(idHazard, strDisplayTxt)
 			eHazardType = tDat.eHazardType
 		end
 	end
-	
+
 	local tHazard = self._DB.profile.tHazards[eHazardType] or self._DB.profile.tHazards["default"]
 
-	self:CreateHazard(idHazard, tHazard.strName, tHazard.nMin, tData.fMaxValue, tHazard.crBar, { strTooltip = tData.strTooltip })
-	
+	self:CreateHazard(idHazard, strDisplayTxt, tHazard.nMin, tData.fMaxValue, tHazard.crBar, { strTooltip = tData.strTooltip })
+
 	self:UpdateHazard(idHazard, { nValue = tData.fMeterValue })
 end
 
@@ -166,7 +186,7 @@ function ForgeUI_Hazards:OnHazardsUpdated()
 		if not self.tHazards[tData.nId] then
 			local tHazard = self._DB.profile.tHazards[tData.eHazardType] or self._DB.profile.tHazards["default"]
 		
-			self:CreateHazard(tData.nId, tHazard.strName, tHazard.nMin, tData.fMaxValue, tHazard.crBar)
+			self:CreateHazard(tData.nId, HazardsLib.GetHazardDisplayString(tData.nId), tHazard.nMin, tData.fMaxValue, tHazard.crBar)
 		end
 		
 		self:UpdateHazard(tData.nId, { nValue = tData.fMeterValue, strTooltip = tData.strTooltip })
