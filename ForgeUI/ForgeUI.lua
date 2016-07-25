f2f287266d427df2287ac52b6c0733a5c42f3afb
Adam Jedlička <adajedlicka@gmail.com>
2016-07-25 10:29:43 +0200

    pairsByKeys function now local

--- ForgeUI/ForgeUI.lua
+++ ForgeUI/ForgeUI.lua
@@ -212,22 +212,27 @@ function Addon:ItemListSignPressed(wndHandler, wndControl, eMouseButton)
 	Inst:SortItemsByPriority()
 end
 
-function pairsByKeys (t, f)
-    local a = {}
-    for n in pairs(t) do
-    	table.insert(a, n)
-    end
-    table.sort(a, f)
-    local i = 0      -- iterator variable
-    local iter = function ()   -- iterator function
-    	i = i + 1
-    	if a[i] == nil then
-    		return nil
-    	else
-    		return a[i], t[a[i]]
-    	end
-    end
-    return iter
+-- Iterates over table in order (alphabetically, ...)
+local function pairsByKeys(t, f)
+	local a = {}
+
+	for n in pairs(t) do
+		table.insert(a, n)
+	end
+
+	table.sort(a, f)
+
+	local i = 0
+	local iter = (function ()
+		i = i + 1
+		if a[i] == nil then
+			return nil
+		else
+			return a[i], t[a[i]]
+		end
+	end)
+
+	return iter
 end
 
 function Addon:SortItemsByPriority()
